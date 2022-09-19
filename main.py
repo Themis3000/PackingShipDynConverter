57d9ac3cc3c5f31c5aec5182e00e4b9efc3cdf4d
Themi <tcm4760@gmail.com>
2022-09-18 23:55:32 -0500

    Now converts multiple pages. Need to fix output.

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from pdfminer.high_level import extract_pages
 from extract_page import extract_page_data
 from page_builder import page_builder, template_values, html_out
 
-page_layout = next(extract_pages("in.pdf"))
+pages = extract_pages("in.pdf")
+html_data = ""
 
-order_data, images = extract_page_data(page_layout)
-print(order_data)
-html_data = page_builder(order_data, images)
+for page in pages:
+    order_data, images = extract_page_data(page)
+    print(order_data)
+    html_data += page_builder(order_data, images)
 
 with open("out.html", "w", encoding="utf-8") as f:
     f.write(template_values(html_out, {"body": html_data}))

7e8b0bc6f7acb3e7cf13afd6874eac5e1d9b7266
Themi <tcm4760@gmail.com>
2022-12-12 02:06:07 -0600

    removed normal output

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from extract_page import extract_page_data
 from page_builder import page_builder, template_values, html_out
 
 pages = extract_pages("in.pdf")
-html_data = ""
 
 for page in pages:
     order_data, images = extract_page_data(page)
@@ -13,6 +12,3 @@ for page in pages:
         f.write(template_values(html_out, {"body": slip_html}))
     with open(f"./out/{order_data['buyer_name']}_label.html", "w", encoding="utf-16") as f:
         f.write(template_values(html_out, {"body": label_html}))
-
-with open("out.html", "w", encoding="utf-16") as f:
-    f.write(template_values(html_out, {"body": html_data}))
